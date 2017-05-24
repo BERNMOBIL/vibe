b3dd82afa998f05cca155ab6d00085935db19021
Oliviero Chiodo <oliviero.chiodo@hsr.ch>
2017-05-24 09:27:41 +0200

    Fix showing higher delay on top

--- src/main/java/ch/bernmobil/vibe/presentationlayer/viewmodel/Converter.java
+++ src/main/java/ch/bernmobil/vibe/presentationlayer/viewmodel/Converter.java
@@ -3,23 +3,33 @@ package ch.bernmobil.vibe.presentationlayer.viewmodel;
 import ch.bernmobil.vibe.dataaccesslayer.entitiy.Schedule;
 import ch.bernmobil.vibe.dataaccesslayer.entitiy.ScheduleUpdate;
 import ch.bernmobil.vibe.dataaccesslayer.entitiy.Stop;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+@Component
 public class Converter {
 
     private static DateTimeFormatter dateTimeFormatter =
-        DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM).withLocale(Locale.GERMAN);
+            DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM).withLocale(Locale.GERMAN);
 
-    public static ScheduleViewModel convertSchedule(Schedule schedule) {
+    public ScheduleViewModel convertSchedule(Schedule schedule) {
         ScheduleViewModel viewModel = new ScheduleViewModel();
         viewModel.setPlannedDeparture(schedule.getPlannedDeparture().format(dateTimeFormatter));
         ScheduleUpdate update = schedule.getScheduleUpdate();
         if (update != null) {
-            viewModel.setActualDeparture(update.getActualDeparture().format(dateTimeFormatter));
+            String delay = getDelay(schedule.getPlannedDeparture(), update.getActualDeparture(), MINUTES);
+            viewModel.setActualDeparture(delay);
         }
         viewModel.setLine(schedule.getJourney().getRoute().getLine());
         viewModel.setDestination(schedule.getJourney().getHeadsign());
@@ -28,17 +38,26 @@ public class Converter {
         return viewModel;
     }
 
-    public static StopViewModel convertStop(Stop stop) {
+    public StopViewModel convertStop(Stop stop) {
         StopViewModel viewModel = new StopViewModel();
         viewModel.setId(stop.getId());
         viewModel.setName(stop.getName());
         return viewModel;
     }
 
-    public static List<ScheduleViewModel> convertScheduleList(List<Schedule> list) {
+    public List<ScheduleViewModel> convertScheduleList(List<Schedule> list) {
         return list
-            .parallelStream()
-            .map(Converter::convertSchedule)
-            .collect(Collectors.toList());
+                .stream()
+                .sorted(Comparator.comparing(Schedule::getPlannedDeparture))
+                .map(this::convertSchedule)
+                .collect(Collectors.toList());
+    }
+
+    private String getDelay(LocalTime planned, LocalTime actual, ChronoUnit unit) {
+        long delay = unit.between(planned, actual);
+        if(delay == 0) {
+            return "OK";
+        }
+        return Long.toString(delay);
     }
 }
